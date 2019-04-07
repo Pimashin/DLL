47ae40cc3baf3d6627c32c1dd86de9dd8a8c1bce
Egor <mimagsnow@gmail.com>
2019-04-07 23:42:01 +0500

    Создана DLL

---
+++ pimashin.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifdef DLL
+#define PIMASHIN __declspec(dllexport)
+#else
+#define PIMASHIN __declspec(dllimport)
+#endif
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+extern "C" PIMASHIN void __stdcall WHOIAM();
---
+++ pimashinDLL.cpp
@@ -0,0 +1,17 @@
+﻿// pimashinDLL.cpp : Определяет экспортированные функции для приложения DLL.
+//
+
+#include "stdafx.h"
+#include <utility>
+#include <limits.h>
+#include <windows.h>
+#include "pimashin.h"
+#include <iostream>
+
+using namespace std;
+
+
+void __stdcall WHOIAM()
+{
+	cout << "Pimashin Egor \nRI541216" << endl;
+}
